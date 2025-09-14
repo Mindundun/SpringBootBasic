7ff04f8c6bfaf73abcf6cd24e52b550441c39ea8
Mindun <pmk10077@gmail.com>
2025-09-14 17:53:08 +0900

    SpringBootBasic

---
+++ springboot/src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String root() {
+        return "Welcome to Spring Boot!#";
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World!!";
+    }
+
+}
