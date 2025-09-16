e3b4240bcd8b46cd187b26ba7a5ac3f5adc2edbb
Mindun <pmk10077@gmail.com>
2025-09-16 17:16:26 +0900

    테스트

--- springboot/src/test/java/com/example/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.springboot;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.springboot.entity.Question;
+import com.example.springboot.repository.QuestionRepository;
 
 @SpringBootTest
 class SpringbootApplicationTests {
+	@Autowired
+	private QuestionRepository questionRepository;
 
 	@Test
-	void contextLoads() {
+	void testJpa(){
+		Question q1 = new Question();
+		q1.setSubject("Spring이 무엇인가요?");
+		q1.setContent("Spring에 대해 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("스프링 부트 모델 질문 입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);
 	}
 
 }
