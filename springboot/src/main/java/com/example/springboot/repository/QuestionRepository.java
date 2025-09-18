5b8acebe5a773b045cdae582827625f0edd2101c
Mindun <pmk10077@gmail.com>
2025-09-18 18:26:08 +0900

    test db data insert, assertEquals

--- springboot/src/main/java/com/example/springboot/repository/QuestionRepository.java
+++ springboot/src/main/java/com/example/springboot/repository/QuestionRepository.java
@@ -6,5 +6,5 @@ import com.example.springboot.entity.Question;
 
 // Question 엔티티로 리포지터리 생성, Question 엔티티의 기본키가 Integer임.
 public interface QuestionRepository extends JpaRepository<Question, Integer>{
-    
+    Question findBySubject(String subject);
 }
--- springboot/src/test/java/com/example/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -1,6 +1,11 @@
 package com.example.springboot;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,17 +21,32 @@ class SpringbootApplicationTests {
 
 	@Test
 	void testJpa(){
-		Question q1 = new Question();
-		q1.setSubject("Spring이 무엇인가요?");
-		q1.setContent("Spring에 대해 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);
+		// 데이터 insert
+		// Question q1 = new Question();
+		// q1.setSubject("Spring이란 무엇인가요?");
+		// q1.setContent("Spring에 대해 알고 싶습니다.");
+		// q1.setCreateDate(LocalDateTime.now());
+		// this.questionRepository.save(q1);
 
-		Question q2 = new Question();
-		q2.setSubject("스프링 부트 모델 질문 입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);
+		// Question q2 = new Question();
+		// q2.setSubject("스프링 부트 모델 질문 입니다.");
+		// q2.setContent("id는 자동으로 생성되나요?");
+		// q2.setCreateDate(LocalDateTime.now());
+		// this.questionRepository.save(q2);
+
+		// data all Query
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		assertEquals("Spring이란 무엇인가요?", q.getSubject());
+
+		// data Query
+		Optional<Question> oq = this.questionRepository.findById(1);
+		if(oq.isPresent()) {
+			Question qQuery = oq.get();
+			assertEquals("스프링 부트 모델 질문 입니다.", qQuery.getSubject());
+		}
 	}
 
 }
