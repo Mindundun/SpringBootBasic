99da4ec37d3f3ca6562bfb21a82fbdbe904e5b33
Mindun <pmk10077@gmail.com>
2025-09-28 18:24:46 +0900

    회원가입 창 생성 및 로직

---
+++ springboot/src/main/java/com/example/user/UserCreateForm.java
@@ -0,0 +1,29 @@
+package com.example.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCreateForm {
+    @Size(min = 3, max = 25)
+    @NotEmpty(message="사용자 ID는 필수 항목입니다.")
+    private String username;
+
+    @NotEmpty(message = "비밀번호는 필수 항목입니다.")
+    private String password1;
+
+    @NotEmpty(message = "비밀번호 확인은 필수 항목입니다.")
+    private String password2;
+
+    @NotEmpty(message = "이메일은 필수 항목입니다.")
+    @Email
+    private String email;
+}
---
+++ springboot/src/main/java/com/example/user/UserService.java
@@ -0,0 +1,24 @@
+package com.example.user;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser create(String username, String email, String password){
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        // 비밀번호 암호화 -> SecurityConfig.java에 Bean 등록
+        user.setPassword(passwordEncoder.encode(password));
+        this.userRepository.save(user);
+        return user;
+    }
+}
