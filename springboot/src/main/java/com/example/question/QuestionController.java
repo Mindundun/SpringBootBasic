82263ce769c82f5e14db80dcb6b1d1c13eb04972
Mindun <pmk10077@gmail.com>
2025-10-07 15:33:19 +0900

    글쓴이 등록 및 로그인페이지로 이동

--- springboot/src/main/java/com/example/question/QuestionController.java
+++ springboot/src/main/java/com/example/question/QuestionController.java
@@ -1,8 +1,9 @@
 package com.example.question;
 
-import java.util.List;
+import java.security.Principal;
 
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.answer.AnswerForm;
+import com.example.user.SiteUser;
+import com.example.user.UserService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +26,7 @@ import lombok.RequiredArgsConstructor;
 public class QuestionController{
     
     private final QuestionService questionService;
+    private final UserService userService;
 
     @GetMapping("/list")
     // @ResponseBody
@@ -39,17 +43,20 @@ public class QuestionController{
         return "question_detail";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/create")
     public String questionCreate(QuestionForm questionForm) {
         return "question_form";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create")
-    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult){
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult, Principal principal){
         if (bindingResult.hasErrors()){
             return "question_form";
         }
-        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        SiteUser siteUser = this.userService.getUser(principal.getName());
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
         return "redirect:/question/list"; // 질문 저장 후 질문 목록으로 이동
     }
 }
